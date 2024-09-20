89012cb83d301d8beac2bcfaf0551de13308b302
spuuntries <kek@spuun.art>
2024-09-20 17:08:52 +0700

    fix: update constraints on simplified

--- daspro-24/prak-1/mainsimplified.py
+++ daspro-24/prak-1/mainsimplified.py
@@ -8,11 +8,11 @@ if M < 1:
     print("Senshi makannya besok aja deh.")
 else:
     if 0 < x < X - 1 and 0 < y < Y - 1:
-        nope = M < 4
+        nope = M < 8
     elif 0 < x < X - 1 or 0 < y < Y - 1:
-        nope = M < 3
+        nope = M < 5
     else:
-        nope = M < 2
+        nope = M < 3
 
 if nope:
     print("Senshi makan hari ini!")
